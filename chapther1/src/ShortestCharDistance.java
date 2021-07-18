e37d7e8caac69c336b35cc9b9069a6dcd6428272
JongyunHa <gkwhddbs77@knou.ac.kr>
2021-07-18 22:26:23 +0900

    가장 짧은 거리 O(2n)

--- chapther1/src/ShortestCharDistance.java
+++ chapther1/src/ShortestCharDistance.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class ShortestCharDistance {
+import java.util.Scanner;
+
+public class ShortestCharDistance {
+    public static  int[] solution(String s, char target) {
+        int[] answer = new int[s.length()];
+        int point = 1000;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == target) {
+                point = 0;
+                answer[i] = point;
+            }
+            else answer[i] = ++point;
+        }
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == target) point = 0;
+            else answer[i] = Math.min(answer[i], ++point);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        char c = in.next().charAt(0);
+        for (int x: solution(s, c)) {
+            System.out.printf("%d ", x);
+        }
+    }
 }
