3cb3779eb1b065493d7e6fdfa6e8ea261e8bbebb
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-02 10:02:32 +0900

    Class president (Hash map)

---
+++ levelup/chapter4/src/classPresident.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class classPresident {
+    public static char solution(String s) {
+        char answer = ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char x : s.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (Map.Entry<Character, Integer> m : map.entrySet()) {
+            if (m.getValue() > max) {
+                max = m.getValue();
+                answer = m.getKey();
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        String voteResult = scanner.next();
+        System.out.println(solution(voteResult));
+    }
+}
